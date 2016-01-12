3922353264274231ddb17b7db89de3bb7668b7a4
Kyle Langley and Mike Mahoney <altck8@gmail.com>
2016-01-12 10:37:19 -0800

    add rspec case for clear

--- lib/places.rb
+++ lib/places.rb
@@ -1,10 +1,23 @@
 class Place
+  @@all_places = []
   define_method(:initialize) do |location|
     @location = location
   end
 
   define_method(:location) do
     return @location
+  end
+
+  define_method(:save) do
+    @@all_places.push(self)
+  end
+
+  define_singleton_method(:all) do
+    return @@all_places
+  end
+
+  define_singleton_method(:clear) do
+    @@all_places = []
   end
 
 end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -3,8 +3,21 @@ require('places')
 require('pry')
 
 describe("#location") do
+  it("will save to class variable") do
+    test_location = Place.new("Salem")
+    test_location.save
+    expect(Place.all).to(eq([test_location]))
+  end
+
   it("will return location") do
     test_location = Place.new("Portland")
     expect(test_location.location).to(eq("Portland"))
+  end
+
+  it("will clear class variable") do
+    test_location = Place.new("Salem")
+    test_location.save
+    Place.clear
+    expect(Place.all).to(eq([]))
   end
 end
